3b3638a91cde62eb98f92eb29b087b5f85750f79
“hemanthreddy31” <hemanthkankanala1@gmail.com>
2024-10-31 13:38:21 +0530

    CreateElement Object

--- Namaste-React/EP01/app.js
+++ Namaste-React/EP01/app.js
@@ -1,4 +1,12 @@
-        //create element takes 3 arugments 
-        const heading = React.createElement('h1', {id:"heading"}, 'Hello World From React');
-        const root = ReactDOM.createRoot(document.getElementById('root'));
-        root.render(heading);
+//create element takes 3 arugments
+//It is an object
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World From React"
+);
+
+console.log(heading); //object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading); //resposnible for converting object to h1 tag and putting in div tag
